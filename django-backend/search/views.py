baffe0da02e3b463578319e6604408c3744cfe0d
Haelles <54494599+Haelles@users.noreply.github.com>
2020-11-29 10:51:06 +0800

    modify author_name to test auto deploy

--- django-backend/search/views.py
+++ django-backend/search/views.py
@@ -10,7 +10,7 @@ def index(request):
 # 搜索作者，返回作者的个人信息
 def search_author(request):
     # author_name = request.POST['author_name']
-    author_name = 'Tom'
+    author_name = 'Henry'
     author = next(scholarly.search_author(author_name))
     res = {'affiliation': author.affiliation,
            'citedby': author.citedby,
